032e1a9ac6d3805fe0f6f3843ef8b78bf6574212
laboratorio <laboratorio@UPOAULA10716>
2025-03-26 07:25:53 -0500

    Agregar archivos de proyecto.

---
+++ FIrstProyect-Class01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FIrstProyect_Class01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
